9cc769bf0553a7dc0d1a096fc9592c822c722b91
Edward <edward@inchbuie.net>
2014-11-11 11:14:10 -0600

    Demonstrating usage in console program

--- FizzBuzzer/Program.cs
+++ FizzBuzzer/Program.cs
@@ -10,9 +10,27 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            FizzBuzzer buzzer = new FizzBuzzer(1, 100);
-            buzzer.CheckRange();
+            // default arguments 
+            //print number in range 1-100, fizz if divisible by 3, buzz if divisible by 5
+            
+            FizzBuzzer buzzer = new FizzBuzzer();
+            var results = buzzer.CheckRange();
+            foreach(var line in results) Console.Write(line);
 
+            Console.WriteLine("Press [Enter] to continue...");
+            Console.ReadLine();
+
+            // custom arguments 
+            //print number in range 3-65, FOUR if divisible by 4, touchdown! if divisible by 7, * if divisible by 9
+            var customParams = new List<DivisorLabel>();
+            customParams.Add(new DivisorLabel(4, "FOUR"));
+            customParams.Add(new DivisorLabel(7, "touchdown"));
+            customParams.Add(new DivisorLabel(9, "*"));
+            FizzBuzzer customBuzzer = new FizzBuzzer(customParams, 3, 65);
+            results = customBuzzer.CheckRange();
+            foreach (var line in results) Console.Write(line);
+
+            Console.WriteLine("Press [Enter] to exit...");
             Console.ReadLine();
         }
     }
